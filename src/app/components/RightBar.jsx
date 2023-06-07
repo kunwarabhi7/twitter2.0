b2e5c7825afbc9906096f877b2b46a1f2236f262
Abhishek <abhishekparmar162@gmail.com>
2023-06-07 15:36:43 +0530

    added react typescript

--- src/app/components/RightBar.jsx
+++ src/app/components/RightBar.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { FcGoogle } from "react-icons/fc";
 import { BsApple } from "react-icons/bs";
+import { FaTwitter } from "react-icons/fa";
 
 const RightBar = () => {
   return (
@@ -23,25 +24,49 @@ const RightBar = () => {
               Sign up with Apple
             </p>
           </button>
-           {/* You can open the modal using ID.showModal() method */}
-      <button
-        className="flex text-black text-sm font-bold items-center justify-center bg-gray-100 w-72 ml-2 py-2 space-x-2 rounded-full"
-        onClick={() => window.my_modal_3.showModal()}
-      >
-        Create account
-      </button>
-      <dialog id="my_modal_3" className="modal">
-        <form method="dialog" className="modal-box">
+          {/* You can open the modal using ID.showModal() method */}
           <button
-            htmlFor="my-modal-3"
-            className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+            className="flex text-black text-sm font-bold items-center justify-center bg-gray-100 w-72 ml-2 py-2 space-x-2 rounded-full"
+            onClick={() => window.my_modal_3.showModal()}
           >
-            ✕
+            Create account
           </button>
-          <h3 className="font-bold text-lg">Hello!</h3>
-          <p className="py-4">Press ESC key or click on ✕ button to close</p>
-        </form>
-      </dialog>
+          <dialog id="my_modal_3" className="modal">
+            <form method="dialog" className="modal-box">
+              <button
+                htmlFor="my-modal-3"
+                className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+              >
+                ✕
+              </button>
+              <div className="flex flex-col space-y-3 items-center">
+                <FaTwitter size={30} className="" />
+                <p>Sign in to Twitter</p>
+                <button className="flex items-center justify-center bg-gray-100 w-72 ml-2 py-2 space-x-2 rounded-full">
+                  <FcGoogle />
+                  <p className="text-black text-sm font-semibold">
+                    Sign up with Google
+                  </p>
+                </button>
+                <button className="flex items-center justify-center bg-gray-100 w-72 ml-2 py-2 space-x-2 rounded-full">
+                  <BsApple className="text-black" />
+                  <p className="text-black text-sm font-semibold">
+                    Sign up with Apple
+                  </p>
+                </button>
+                <div class="inline-flex items-center justify-center w-full">
+                  <hr class="w-64 h-1 my-8 bg-gray-200 border-0 rounded dark:bg-gray-700" />
+                  <div class="absolute px-4 -translate-x-1/2 bg-white left-1/2 dark:bg-gray-900">
+                    or
+                  </div>
+                </div>
+                  <div className="flex flex-col space-y-2">
+                    <input className="border p-2 px-7 w-64   hover:outline-offset-1  hover:outline-blue-400  rounded-md bg-transparent text-white" placeholder="Phone, email, or username" />
+                    <input className="border p-2 px-7 w-64" placeholder="Password" />
+                  </div>
+              </div>
+            </form>
+          </dialog>
         </div>
         <p className="text-xs m-2 text-gray-500">
           By signing up, you agree to the
@@ -72,7 +97,6 @@ const RightBar = () => {
           @ 2023 X Corp.
         </button>
       </div>
-     
     </div>
   );
 };
